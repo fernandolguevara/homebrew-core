b038dad8cf0b8d037e3fa6038b1e117cf8b76170
Sean Molenaar <SMillerDev@users.noreply.github.com>
2024-09-02 17:12:24 +0200

    Update aider.rb

--- Formula/a/aider.rb
+++ Formula/a/aider.rb
@@ -427,7 +427,8 @@ class Aider < Formula
       assert_match version.to_s, shell_output("#{bin}/aider --version")
       assert_match "Missing these environment variables", shell_output("#{bin}/aider --yes --exit --no-check-update")
       ENV["OPENAI_API_KEY"] = "invalid"
-      assert_match "Incorrect API key", shell_output("#{bin}/aider --yes --exit --message=test --no-check-update 2>&1")
+      output = shell_output("#{bin}/aider --yes --exit --message=test --no-check-update 2>&1")
+      assert_match "Incorrect API key", output
     end
   end
 end
